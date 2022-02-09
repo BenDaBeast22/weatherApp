0de8a81ee5722530ce0e9cd6602d6db021e28f22
Benjamin Critoph <benjamincritoph@Benjamins-MacBook-Air.local>
2022-02-09 18:41:30 -0500

    adjust deploy

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,9 @@ npm run build
 
 # navigate into the build output directory
 cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:username/hello-world.git master:gh-pages
+cd -
